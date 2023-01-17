2172abaa6c3a4105fdbb97526016dc82875f7c9e
Carla Godínez <carlareyesgo@gmail.com>
2023-01-16 17:46:57 -0700

    Primer Commit

---
+++ pokeudx/src/App.js
@@ -0,0 +1,11 @@
+import Searcher from './components/Searcher';
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <Searcher />
+    </div>
+  );
+}
+
+export default App;
---
+++ pokeudx/src/components/Searcher.jsx
@@ -0,0 +1,7 @@
+import {Input} from 'antd';
+
+const Searcher = () => {
+    return <Input.Searcher placeholder= 'Buscar...'/>;
+};
+
+export default Searcher;
